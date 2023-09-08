c8886d392294a4e0eefa52d046c0fa26f99425bc
Arthur Daleprani <102961025+arthurdaleprani@users.noreply.github.com>
2023-09-08 00:25:27 -0300

    Add files via upload

--- Pages/PageComida/ComidaJaponesaPage.js
+++ Pages/PageComida/ComidaJaponesaPage.js
@@ -1,17 +1,54 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Image, ActivityIndicator, Buutton, StyleSheet } from 'react-native';
+import { View, Text, Image, ActivityIndicator, Button, StyleSheet } from 'react-native';
 
-const Japa = {
+const Japa = [
+ {
+    nome: "Kizoko Sushi",
+    foto: require('../../src/imgs/japa1.PNG'),
+    desc: 'A culinária japonesa contemporânea e do tradicional repaginado em buffet descontraído de iguarias chamativas.',
+    localizacao:'https://goo.gl/maps/ipHTPhkaJkzLKJs47'
+
+ },
+
+ {
+  nome: "Mori yah Sushi",
+  foto: require('../../src/imgs/japa2.PNG'),
+  desc: 'Com um novo conceito em culinária japonesa, estamos trazendo o melhor do sushi contemporânea com preço acessível.',
+  localizacao:'https://goo.gl/maps/McZ4MYs1uXLABqHU8'
+
+},
+{
+  nome: "Kokai Sushi",
+  foto: require('../../src/imgs/japa3.PNG'),
+  desc: 'O Kokai Sushi Praia da Costa é um restaurante japonês renomado na Praia da Costa, conhecido por sua autêntica culinária japonesa e ambiente à beira-mar. Oferece uma variedade de pratos de sushi e sashimi de alta qualidade.',
+  localizacao:'https://goo.gl/maps/E2p7oEPqHR6oEEUd7'
+
+},
+{
+  nome: "Potemaki San Sushi",
+  foto: require('../../src/imgs/japa4.PNG'),
+  desc: 'O sabor do oriente na sua mesa',
+  localizacao:'https://goo.gl/maps/GfGkaxxiwZVC87ks9'
+
+},
+{
+  nome: "Sushi Trip",
+  foto: require('../../src/imgs/japa5.PNG'),
+  desc: 'Sushi Trip é um restaurante que oferece uma jornada gastronômica única pela culinária japonesa. Com uma variedade de pratos de sushi e sashimi de alta qualidade, proporciona aos clientes uma experiência de sabores autênticos do Japão em um ambiente acolhedor e convidativo',
+  localizacao:'https://goo.gl/maps/BPkZXRvRjthKn9Hr7'
+
+},
 
 
 
-}
+
+]
 
 function japaEscolher() {
-indiceAleatorio = Math.floor(Math.random() * Japa.lenght)
-return Japa[indiceAleatorio]
-
+  const indiceAleatorio = Math.floor(Math.random() * Japa.length); // Corrigido "lenght" para "length"
+  return Japa[indiceAleatorio];
 }
+
 
 
 
@@ -22,14 +59,9 @@ const ComidaJaponesaPage = () => {
   
   useEffect(()=>{
       setTimeout(()=>{
-          const Japa = japaEscolher();
-          setJapaAleatorio(Japa);
+          setJapaAleatorio(japaEscolher());
           setIsLoading(false);
-
-
       }, 1000)
-
-
   }, [])
   
   
@@ -42,13 +74,14 @@ const ComidaJaponesaPage = () => {
       ): japaAleatorio ? (
         <View>
           <Image source={japaAleatorio.foto} style={styles.foto}/>
-          <Text source={japaAleatorio.nome} style={styles.nome}/>
-          <Text source={japaAleatorio.desc} style={styles.desc}/>
-          <Button style={styles.button}
+          <Text style={styles.nome}>{japaAleatorio.nome}</Text>
+          <Text style={styles.desc}>{japaAleatorio.desc}</Text>
+          <Button
+            style={styles.button}
             title="Ver Localização"
             onPress={() => {
               // Abra a localização no Google Maps ou outro aplicativo de mapa
-              // Use restauranteAleatorio.localizacao para o link
+              // Use japaAleatorio.localizacao para o link
             }}
           />
         </View>
@@ -59,32 +92,28 @@ const ComidaJaponesaPage = () => {
 
 
 const styles = StyleSheet.create({
-container:{
-
-
-
-},
-
-foto:{
-
-
-},
-
-nome:{
-
-
-
-
-},
-
-desc:{
-
-
-
-
-},
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  foto: {
+    width: 200,
+    height: 200,
+    borderRadius: 10,
+  },
+  nome: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  desc: {
+    fontSize: 16,
+    marginTop: 10,
+  },
 button:{
-
+width:30,
+height:30
 
 
   
--- Pages/escolherTipo.js
+++ Pages/escolherTipo.js
@@ -1,33 +1,72 @@
 import React from 'react';
-import { View, Button } from 'react-native';
-import ChurrasquinhoPage from './PageComida/ChurrasquinhoPage';
-
-
+import { View, TouchableOpacity, Image, Text, StyleSheet } from 'react-native';
 
 const EscolherTipoScreen = ({ navigation, route }) => {
-   const { irPag } = route.params;
+  const { irPag } = route.params;
 
-   return (
-      <View>
-         <Button
-             title="Churrasquinho"
-             onPress={() => navigation.navigate('ChurrasquinhoPage', { mostrarDadosRestaurante: true })}
-          
-         />
-         <Button
-            title="Pizza"
-            onPress={() => navigation.navigate('PizzaPage')}
-         />
-         <Button
-            title="Hamburguer"
-            onPress={() => navigation.navigate('HamburguerPage')}
-         />
-         <Button
-            title="Comida Japonesa"
-            onPress={() => navigation.navigate('ComidaJaponesaPage')}
-         />
-      </View>
-   );
+  const buttonData = [
+    {
+      title: "Churrasquinho",
+      image: require('../src/imgs/espetin.jpg'), 
+      onPress: () => navigation.navigate('ChurrasquinhoPage', { mostrarDadosRestaurante: true }),
+    },
+    {
+      title: "Pizza",
+      image: require('../src/imgs/logopizza.jpg'), 
+      onPress: () => navigation.navigate('PizzaPage'),
+    },
+    {
+      title: "Hamburguer",
+      image: require('../src/imgs/burguer.jpg'), 
+      onPress: () => navigation.navigate('HamburguerPage'),
+    },
+    {
+      title: "Comida Japonesa",
+      image: require('../src/imgs/japa.jpg'), 
+      onPress: () => navigation.navigate('ComidaJaponesaPage', { mostrarDadosRestaurante: true }),
+    },
+  ];
+
+  return (
+    <View style={styles.container}>
+      {buttonData.map((button, index) => (
+        <TouchableOpacity
+          key={index}
+          style={styles.button}
+          onPress={button.onPress}
+        >
+          <Image source={button.image} style={styles.buttonImage} />
+          <Text style={styles.buttonText}>{button.title}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#E07F21',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  buttonImage: {
+    width: 24,
+    height: 24,
+    marginRight: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
 
 export default EscolherTipoScreen;
